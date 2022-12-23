40c3f7e5a44cf6d83997476faf620d890cf24dfe
anthonwellsjo <anthonwellsjo@gmail.com>
2022-12-23 23:24:20 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,10 @@ pub fn get_args() -> Vec<String> {
     let args: Vec<String> = std::env::args().collect();
     args[1..].to_vec()
 }
+
+pub fn get_argument_at(index: usize) -> Option<String> {
+    match std::env::args().collect::<Vec<String>>().get(index){
+        Some(arg) => Some(*arg),
+        None => todo!(),
+    }
+}
