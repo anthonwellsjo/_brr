d3acee2f9c748ee67d68bd58db9aa8b3c985042e
anthonwellsjo <anthonwellsjo@gmail.com>
2023-01-12 17:21:25 +0100

    chore: cleaning for release

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::process::Command;
-use sysinfo::{ProcessExt, System, SystemExt, PidExt};
+use sysinfo::{PidExt, ProcessExt, System, SystemExt};
 
 pub fn get_args() -> Vec<String> {
     let args: Vec<String> = std::env::args().collect();
@@ -26,21 +26,29 @@ pub fn get_user() -> String {
     user
 }
 
-pub struct ProcessCount{
-    pid: Vec<u32>,
-    name: String
+#[derive(Debug)]
+pub struct ProcessCount {
+    pub pid: Vec<u32>,
+    pub name: String,
 }
 
 impl ProcessCount {
-    pub fn new(name: &str) -> Self {ProcessCount{name: name.to_string(), pid: Vec::<u32>::new()}}
+    pub fn new(name: &str) -> Self {
+        ProcessCount {
+            name: name.to_string(),
+            pid: Vec::<u32>::new(),
+        }
+    }
 }
 
-pub fn get_processes(name: &str) -> ProcessCount{
+pub fn get_processes(name: &str, excl_current: bool) -> ProcessCount {
     let s = System::new_all();
     let mut p_count = ProcessCount::new(name);
     for process in s.processes_by_exact_name(&name) {
+        if excl_current && std::process::id() == process.pid().as_u32() {
+            continue;
+        }
         p_count.pid.push(process.pid().as_u32());
-        println!("{} {}", process.pid(), process.name());
     }
 
     p_count
